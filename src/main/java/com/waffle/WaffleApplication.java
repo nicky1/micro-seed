27981957c2e534c6d2f8d7f24cbb36a6f810e8ec
nick3 <xiao8313020@163.com>
2018-02-06 11:47:42 +0800

    1.初始化入口类。

--- src/main/java/com/waffle/WaffleApplication.java
+++ src/main/java/com/waffle/WaffleApplication.java
@@ -7,20 +7,16 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ImportResource;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
-
 @SpringBootApplication
 @ImportResource("classpath:beanRefContext.xml")
 @EnableDiscoveryClient
 @EnableAutoConfiguration
 @EnableFeignClients
-public class Application {
+public class WaffleApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(WaffleApplication.class, args);
 	}
 
 }
