75804bed164032c323a2eb1ef5f4ea4d338d6e8d
易晓双 <yixiaoshuang@zhongan.com>
2018-06-13 14:58:37 +0800

    spring boot version to 2.0.0

--- src/main/java/com/waffle/WaffleApplication.java
+++ src/main/java/com/waffle/WaffleApplication.java
@@ -1,17 +1,9 @@
 package com.waffle;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:beanRefContext.xml")
-@EnableDiscoveryClient
-@EnableAutoConfiguration
-@EnableFeignClients
 public class WaffleApplication {
 
 	public static void main(String[] args) {
