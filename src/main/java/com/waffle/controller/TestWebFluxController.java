b8be19e86f44f6722447b3ccb4d7d6e10df9fb6e
易晓双 <yixiaoshuang@yeah.net>
2020-03-13 23:35:27 +0800

    [feat] : 1.mysql账号配置

--- src/main/java/com/waffle/controller/TestWebFluxController.java
+++ src/main/java/com/waffle/controller/TestWebFluxController.java
@@ -4,8 +4,10 @@ import com.waffle.model.mongodb.Work;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Mono;
 
 import javax.annotation.Resource;
 
@@ -13,7 +15,7 @@ import javax.annotation.Resource;
  * @author yixiaoshuang
  * @date 2019-10-11 23:20
  */
-@RestController
+@Controller
 public class TestWebFluxController {
 
     @Resource
@@ -23,13 +25,19 @@ public class TestWebFluxController {
     private ReactiveMongoTemplate reactiveMongoTemplate;
 
     @GetMapping(value = "/api/webflux/test/save")
-    public ResponseEntity save() {
+    public @ResponseBody
+    ResponseEntity save() {
 
-        Work work = Work.builder().name("张三111").createTime(System.currentTimeMillis()).build();
-        mongoTemplate.save(work);
-
-        Work work2 = Work.builder().name("张三222").createTime(System.currentTimeMillis()).build();
+        Work work2 = Work.builder().name("张三422234").createTime(System.currentTimeMillis()).build();
         reactiveMongoTemplate.insert(work2).subscribe();
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/api/webflux/test/reactive/save")
+    public @ResponseBody
+    Mono reSave() {
+
+        Work work2 = Work.builder().name("张三2223456").createTime(System.currentTimeMillis()).build();
+        return reactiveMongoTemplate.insert(work2);
     }
 }
